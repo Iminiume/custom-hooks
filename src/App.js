77748c5d64de4f1fa18886d8cc79c1fdb84f4f9f
User <user@Amins-MacBook-Pro.local>
2023-09-09 14:56:20 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import useGenerateUuid from "./hooks/useGenerateUuid";
+import useSetCookie from "./hooks/useSetCookie";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const uuid = useGenerateUuid();
+  const cookieName = "_uuid";
+  const daysToExpire = 7;
+
+  useSetCookie(cookieName, uuid, daysToExpire);
+
+  return <div>App</div>;
 }
 
 export default App;
---
+++ src/hooks/useGenerateUuid.js
@@ -0,0 +1,23 @@
+export default function generateUUID() {
+  function randomHex(length) {
+    let result = "";
+    for (let i = 0; i < length; i++) {
+      result += Math.floor(Math.random() * 16).toString(16);
+    }
+    return result;
+  }
+
+  const timePart = randomHex(8);
+  const clockSeqPart = randomHex(4);
+  const nodePart = randomHex(12);
+
+  const version = "4";
+
+  const variant = "8";
+  const uuid = `${timePart}-${clockSeqPart}-${version}${nodePart.substr(
+    0,
+    3
+  )}-${variant}${nodePart.substr(3)}`;
+
+  return uuid;
+}
